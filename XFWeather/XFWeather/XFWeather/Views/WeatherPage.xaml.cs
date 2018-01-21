f3cf106d4a0cc655745935a5324376cf2d5d189d
Sebastian Jensen <apps@tsjdev-apps.de>
2018-01-21 14:51:45 +0100

    - add weather- and forecastpage

---
+++ XFWeather/XFWeather/XFWeather/Views/WeatherPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFWeather.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.iOS)
+                Icon = new FileImageSource { File = "tab1.png" };
+        }
+    }
+}
