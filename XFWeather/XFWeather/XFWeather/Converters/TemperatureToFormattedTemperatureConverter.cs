0435c5146f51b4cf1ed326090654177454e98a85
Sebastian Jensen <apps@tsjdev-apps.de>
2018-01-21 15:38:13 +0100

    - add converter to show better forecast

---
+++ XFWeather/XFWeather/XFWeather/Converters/TemperatureToFormattedTemperatureConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using CommonServiceLocator;
+using Xamarin.Forms;
+using XFWeather.ViewModels;
+
+namespace XFWeather.Converters
+{
+    public class TemperatureToFormattedTemperatureConverter : IValueConverter
+    {
+        private readonly WeatherViewModel _weatherViewModel;
+
+        public TemperatureToFormattedTemperatureConverter()
+        {
+            _weatherViewModel = ServiceLocator.Current.GetInstance<WeatherViewModel>();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var units = _weatherViewModel.IsMetric ? "C" : "F";
+            return $"Temperature: {value ?? 0}° {units}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
