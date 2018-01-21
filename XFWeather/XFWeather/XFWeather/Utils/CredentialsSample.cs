84bc2807c918d978d15972f1f56c26711b460067
Sebastian Jensen <apps@tsjdev-apps.de>
2018-01-21 14:43:26 +0100

    - add sample class for api key

---
+++ XFWeather/XFWeather/XFWeather/Utils/CredentialsSample.cs
@@ -0,0 +1,13 @@
+﻿namespace XFWeather.Utils
+{
+    /// <summary>
+    /// Sample Class to provide the Open Weather Map api key.
+    /// </summary>
+    public static class CredentialsSample
+    {
+        /// <summary>
+        /// Please see <see href="https://home.openweathermap.org/api_keys">HERE</see> to get one.
+        /// </summary>
+        public static string ApiKey = "yourkeyhere";
+    }
+}
